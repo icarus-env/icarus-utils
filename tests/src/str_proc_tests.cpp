8edb34ea35dd05b8593d8c70a01aa183cc6e4058
Houssem Guissouma <houssem.guissouma@kit.edu>
2024-10-13 18:43:11 +0200

    Add one test case for string processing module.

--- tests/src/str_proc_tests.cpp
+++ tests/src/str_proc_tests.cpp
@@ -10,7 +10,7 @@
 // Module under Test
 #include "icarus/utils/str_processing.h"
 
-using namespace icarus;
+using namespace icarus::utils;
 
 namespace tests {
 
@@ -20,19 +20,19 @@ namespace tests {
 TEST(StrProcTests, TrimStr) {
 	// Test string with leading and trailing whitespace
 	std::string test_str = "  Hello, World!  ";
-	ASSERT_EQ(utils::trimStr(test_str), "Hello, World!");
+	ASSERT_EQ(trimStr(test_str), "Hello, World!");
 
 	// Test string with leading whitespace
 	test_str = "    Hi";
-	ASSERT_EQ(utils::trimStr(test_str), "Hi");
+	ASSERT_EQ(trimStr(test_str), "Hi");
 
 	// Test string with trailing whitespace
 	test_str = "Bye    ";
-	ASSERT_EQ(utils::trimStr(test_str), "Bye");
+	ASSERT_EQ(trimStr(test_str), "Bye");
 	
 	// Test string with only whitespaces
 	test_str = "    ";
-	ASSERT_EQ(utils::trimStr(test_str), "");
+	ASSERT_EQ(trimStr(test_str), "");
 }
 
 /**
@@ -42,7 +42,7 @@ TEST(StrProcTests, RemoveStringsFromVector) {
 	// Test data 1
 	std::vector<std::string> to_remove = { "Hello", "World" };
 	std::vector<std::string> source = { "Hello", "World", "Hi", "Bye" };
-	utils::removeStringsFromVector(source, to_remove);
+	removeStringsFromVector(source, to_remove);
 	ASSERT_EQ(source.size(), 2);
 	ASSERT_EQ(source[0], "Hi");
 	ASSERT_EQ(source[1], "Bye");
@@ -50,7 +50,7 @@ TEST(StrProcTests, RemoveStringsFromVector) {
 	// Test data 2
 	to_remove = { "Hi", "Bye" };
 	source = { "Bye", "Hello", "World", "Hi", "Bye", "Hi" };
-	utils::removeStringsFromVector(source, to_remove);
+	removeStringsFromVector(source, to_remove);
 	ASSERT_EQ(source.size(), 2);
 	ASSERT_EQ(source[0], "Hello");
 	ASSERT_EQ(source[1], "World");
@@ -58,7 +58,7 @@ TEST(StrProcTests, RemoveStringsFromVector) {
 	// Test data 3
 	to_remove = { "Hello", "World", "Hi", "Bye" };
 	source = { "Hello", "World", "Hi", "Bye" };
-	utils::removeStringsFromVector(source, to_remove);
+	removeStringsFromVector(source, to_remove);
 	ASSERT_EQ(source.size(), 0);
 }
 
@@ -70,7 +70,7 @@ TEST(StrProcTests, PrintIndentedString) {
 	std::string message = "Hello, World!";
 	std::string indent = "    ";
 	testing::internal::CaptureStdout();
-	utils::printIndentedString(message, indent);
+	printIndentedString(message, indent);
 	std::string output = testing::internal::GetCapturedStdout();
 	ASSERT_EQ(output, "    Hello, World!\n");
 
@@ -78,7 +78,7 @@ TEST(StrProcTests, PrintIndentedString) {
 	message = "Hello, World!\nHow are you?";
 	indent = "    ";
 	testing::internal::CaptureStdout();
-	utils::printIndentedString(message, indent);
+	printIndentedString(message, indent);
 	output = testing::internal::GetCapturedStdout();
 	ASSERT_EQ(output, "    Hello, World!\n    How are you?\n");
 
@@ -86,9 +86,29 @@ TEST(StrProcTests, PrintIndentedString) {
 	message = "Hello, World!\nHow are you?\nI am fine.";
 	indent = "    ";
 	testing::internal::CaptureStdout();
-	utils::printIndentedString(message, indent);
+	printIndentedString(message, indent);
 	output = testing::internal::GetCapturedStdout();
 	ASSERT_EQ(output, "    Hello, World!\n    How are you?\n    I am fine.\n");
+}
+
+/**
+ * @test Tests the functions containsWhitespace and containsSpecialChars.
+ */
+TEST(StrProcTests, StrContainsChars) {
+	// Test data 1
+	std::string test_str = "Hello, World!";
+	ASSERT_TRUE(containsWhitespace(test_str));
+	ASSERT_TRUE(containsSpecialChars(test_str));
+
+	// Test data 2
+	test_str = "Hello";
+	ASSERT_FALSE(containsWhitespace(test_str));
+	ASSERT_FALSE(containsSpecialChars(test_str));
+
+	// Test data 3
+	test_str = "Hello@?";
+	ASSERT_FALSE(containsWhitespace(test_str));
+	ASSERT_TRUE(containsSpecialChars(test_str));
 }
 
 } // namespace tests
